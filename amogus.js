2c922ef615dc9fc0d5d07bc57834385f01387f46
joebamba <72848253+joebamba@users.noreply.github.com>
2021-03-20 17:18:46 +1000

    add images

--- amogus.js
+++ amogus.js
@@ -3,6 +3,6 @@ const body = document.body.style;
 
 function imposter() {
   sus = !sus;
-  body.fontFamily = sus ? "amogus" : "serif";
+  body.fontFamily = sus ? "amogus" : "var(--mainFont)";
   body.textTransform = sus ? "uppercase" : "none";
 }
